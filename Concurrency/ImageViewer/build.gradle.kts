43c0cab2b00f2fbd99ae1357e8be177b79724efc
Paulo Pereira <palbp.giga@gmail.com>
2022-03-03 18:26:49 +0000

    Fixes build of ImageViewer desktop compose application

--- Concurrency/ImageViewer/build.gradle.kts
+++ Concurrency/ImageViewer/build.gradle.kts
@@ -26,7 +26,7 @@ tasks.withType<KotlinCompile> {
 
 compose.desktop {
     application {
-        mainClass = "MainKt"
+        mainClass = "palbp.laboratory.imageviewer.MainKt"
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "ImageViewer"
