631cb9681922e461f57ff7c8e991285c28e9a940
Paulo Pereira <palbp.giga@gmail.com>
2022-05-24 16:17:54 +0100

    Adds cleanup of inactive sessions and orderly server shutdown to Echo Server

--- Concurrency/EchoServer/src/main/kotlin/palbp/laboratory/echo/coroutines/Main_coroutines_throttled.kt
+++ Concurrency/EchoServer/src/main/kotlin/palbp/laboratory/echo/coroutines/Main_coroutines_throttled.kt
@@ -1,9 +1,15 @@
 package palbp.laboratory.echo.coroutines
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import palbp.laboratory.echo.AsyncSemaphore
 import java.net.Socket
@@ -42,27 +48,26 @@ fun main(args: Array<String>) {
 
     val throttle = AsyncSemaphore(2)
 
-    val scope = CoroutineScope(executor.asCoroutineDispatcher())
-    scope.launch {
+    val serverLoopJob = CoroutineScope(executor.asCoroutineDispatcher()).launch {
         while(true) {
             throttle.acquire().await()
             logger.info("Ready to accept connections")
             val sessionSocket = serverSocket.suspendingAccept()
             launch {
-                try {
-                    handleEchoSession(sessionSocket)
-                }
-                finally {
-                    throttle.release()
-                }
+                try { handleEchoSession(sessionSocket) }
+                finally { throttle.release() }
             }
         }
     }
 
+    // This emulates the server console loop
     readln()
 
-    logger.info("Shutting down...")
-    scope.cancel()
+    logger.info("Initiating shutting down...")
+    runBlocking {
+        serverLoopJob.cancelAndJoin()
+    }
+
     executor.shutdown()
     val terminated = executor.awaitTermination(5, TimeUnit.SECONDS)
     logger.info("Done. Executor terminated successfully? $terminated")
@@ -76,25 +81,42 @@ private suspend fun handleEchoSession(sessionSocket: AsynchronousSocketChannel) 
     var echoCount = 0
 
     sessionSocket.use {
-        logger.info("Accepted session $sessionId")
-        it.suspendingWriteLine("Welcome client number $sessionId!")
-        it.suspendingWriteLine("I'll echo everything you send me. Finish with '$EXIT'. Ready when you are!")
+        try {
+            logger.info("Accepted session $sessionId")
+            it.suspendingWriteLine("Welcome client number $sessionId!")
+            it.suspendingWriteLine("I'll echo everything you send me. Finish with '$EXIT'. Ready when you are!")
 
-        while (true) {
-            val line = it.suspendingReadLine()
-            if (line == EXIT)
-                break
-            logger.info("Received line number '${++echoCount}'. Echoing it.")
-            it.suspendingWriteLine("($echoCount) Echo: $line")
+            while (true) {
+                when (val line = it.suspendingReadLine(5, TimeUnit.MINUTES)) {
+                    EXIT -> {
+                        it.suspendingWriteLine("Bye!")
+                        break
+                    }
+                    null -> {
+                        it.suspendingWriteLine("Session has been idle for too long. Terminating.")
+                        break
+                    }
+                    else -> {
+                        logger.info("Received line number '${++echoCount}'. Echoing it.")
+                        it.suspendingWriteLine("($echoCount) Echo: $line")
+                    }
+                }
+            }
         }
-
-        it.suspendingWriteLine("Bye!")
-        logger.info("Session $sessionId ends")
+        catch (cancelled: CancellationException) {
+            logger.error("Session cancelled", cancelled)
+            withContext(NonCancellable) {
+                it.suspendingWriteLine("Server is shutting down. Service will be resumed later.")
+            }
+        }
+        finally {
+            logger.info("Session $sessionId ends")
+        }
     }
 }
 
 suspend fun <T> CompletableFuture<T>.await(): T {
-    return suspendCoroutine { continuation ->
+    return suspendCancellableCoroutine { continuation ->
         whenComplete { result, error ->
             if (error != null)
                 continuation.resumeWithException(error)
--- Concurrency/EchoServer/src/main/kotlin/palbp/laboratory/echo/coroutines/NIO_extensions.kt
+++ Concurrency/EchoServer/src/main/kotlin/palbp/laboratory/echo/coroutines/NIO_extensions.kt
@@ -1,16 +1,22 @@
 package palbp.laboratory.echo.coroutines
 
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.suspendCancellableCoroutine
 import org.slf4j.LoggerFactory
 import java.net.InetSocketAddress
 import java.nio.ByteBuffer
 import java.nio.CharBuffer
 import java.nio.channels.AsynchronousChannelGroup
+import java.nio.channels.AsynchronousCloseException
 import java.nio.channels.AsynchronousServerSocketChannel
 import java.nio.channels.AsynchronousSocketChannel
 import java.nio.channels.CompletionHandler
+import java.nio.channels.InterruptedByTimeoutException
 import java.nio.charset.CharsetDecoder
 import java.nio.charset.CharsetEncoder
 import java.util.concurrent.ExecutorService
+import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -28,7 +34,7 @@ fun createServerChannel(hostname: String, port: Int, executor: ExecutorService):
 }
 
 suspend fun AsynchronousServerSocketChannel.suspendingAccept(): AsynchronousSocketChannel {
-    return suspendCoroutine { continuation ->
+    return suspendCancellableCoroutine { continuation ->
         accept(null, object : CompletionHandler<AsynchronousSocketChannel, Any?> {
             override fun completed(socketChannel: AsynchronousSocketChannel, attachment: Any?) {
                 //logger.info("Accept completed. Cancelled = ${continuation.isCancelled}")
@@ -45,39 +51,54 @@ suspend fun AsynchronousServerSocketChannel.suspendingAccept(): AsynchronousSock
 
 
 suspend fun AsynchronousSocketChannel.suspendingWriteLine(text: String): Int {
-    return suspendCoroutine { continuation ->
+    return suspendCancellableCoroutine { continuation ->
         val toSend = CharBuffer.wrap(text + "\n")
         // This is NOT production ready! It's a DEMO!
         // E.g. We would need to deal with the case when not all the string's chars are written in one call
         write(encoder.encode(toSend), null, object : CompletionHandler<Int, Any?> {
             override fun completed(result: Int, attachment: Any?) {
-                //logger.info("Write succeeded. Cancelled = ${continuation.isCancelled}")
-                continuation.resume(result)
+                if (continuation.isCancelled)
+                    continuation.resumeWithException(CancellationException())
+                else
+                    continuation.resume(result)
             }
 
             override fun failed(error: Throwable, attachment: Any?) {
-                //logger.error("Write failed. Cancelled = ${continuation.isCancelled}")
                 continuation.resumeWithException(error)
             }
         })
     }
 }
 
-suspend fun AsynchronousSocketChannel.suspendingReadLine(): String {
-    return suspendCoroutine { continuation ->
+/**
+ * Reads a line from this socket channel.
+ *
+ * @param timeout   The maximum time for the I/O operation to complete
+ * @param unit      The time unit of the {@code timeout} argument
+ * @return the read line, or null if the operation timed out
+ */
+suspend fun AsynchronousSocketChannel.suspendingReadLine(timeout: Long = 0, unit: TimeUnit = TimeUnit.MILLISECONDS): String? {
+    return suspendCancellableCoroutine { continuation ->
         val buffer = ByteBuffer.allocate(1024)
         // This is NOT production ready! It's a DEMO!
         // E.g. We would need to deal with the case when read does not contain the whole line
-        read(buffer, null, object : CompletionHandler<Int, Any?> {
+        read(buffer, timeout, unit, null, object : CompletionHandler<Int, Any?> {
             override fun completed(result: Int, attachment: Any?) {
-                //logger.info("Read succeeded. Cancelled = ${continuation.isCancelled}")
-                val received = decoder.decode(buffer.flip()).toString().trim()
-                continuation.resume(received)
+                if (continuation.isCancelled)
+                    continuation.resumeWithException(CancellationException())
+                else {
+                    val received = decoder.decode(buffer.flip()).toString().trim()
+                    continuation.resume(received)
+                }
             }
 
             override fun failed(error: Throwable, attachment: Any?) {
-                //logger.error("Read failed. Cancelled = ${continuation.isCancelled}")
-                continuation.resumeWithException(error)
+                if (error is InterruptedByTimeoutException) {
+                    continuation.resume(null)
+                }
+                else {
+                    continuation.resumeWithException(error)
+                }
             }
         })
     }
