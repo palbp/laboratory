e3a4c3ce89800bceb2128098885b3087ed7e9f9d
Paulo Pereira <palbp.giga@gmail.com>
2022-06-14 22:11:19 +0100

    Adds step 7 of the code lab Jetpack Compose Basics.

--- Mobile/Android_Codelabs/ComposeBasicsCodelab/app/src/main/java/palbp/laboratory/codelabs/composebasics/MainActivity.kt
+++ Mobile/Android_Codelabs/ComposeBasicsCodelab/app/src/main/java/palbp/laboratory/codelabs/composebasics/MainActivity.kt
@@ -5,13 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -45,21 +44,25 @@ fun MyApp(names: List<String>) {
 
 @Composable
 fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello,")
                 Text(text = "$name!")
             }
             
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            OutlinedButton(onClick = { expanded = !expanded }) {
+                if (expanded) Text(text = "Show less") else Text(text = "Show more")
             }
         }
-
     }
 }
 
