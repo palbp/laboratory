558545025a07e612a5faf5c6b119ddcdaa397abd
Paulo Pereira <palbp.giga@gmail.com>
2022-06-15 13:41:58 +0100

    Adds finishing touches to the code lab Jetpack Compose Basics solution.

--- Mobile/Android_Codelabs/ComposeBasicsCodelab/app/src/main/java/palbp/laboratory/codelabs/composebasics/GreetingsScreen.kt
+++ Mobile/Android_Codelabs/ComposeBasicsCodelab/app/src/main/java/palbp/laboratory/codelabs/composebasics/GreetingsScreen.kt
@@ -1,23 +1,23 @@
 package palbp.laboratory.codelabs.composebasics
 
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import palbp.laboratory.codelabs.composebasics.ui.theme.ComposeBasicsCodelabTheme
 
@@ -38,33 +38,55 @@ fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 
 @Composable
 fun Greeting(name: String) {
-    var expanded by remember { mutableStateOf(false) }
-    val extraPadding by animateDpAsState(
-        targetValue = if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
-    Surface(
-        color = MaterialTheme.colors.primary,
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
-            ) {
-                Text(text = "Hello,")
-                Text(
-                    text = name,
-                    style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
-                )
-            }
+        CardContent(name = name)
+    }
+}
 
-            OutlinedButton(onClick = { expanded = !expanded }) {
-                if (expanded) Text(text = "Show less") else Text(text = "Show more")
-            }
+@Composable
+fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello,")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                )
+            )
+
+            if (expanded)
+                Text(text = "Codex, qui demonstrat usum tabularum pigrorum " +
+                        "in componendis cum elementis animatis.".repeat(4))
+        }
+
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector =
+                if (expanded) Icons.Filled.KeyboardArrowUp
+                else Icons.Filled.KeyboardArrowDown,
+                contentDescription =
+                if (expanded) stringResource(id = R.string.show_less)
+                else stringResource(id = R.string.show_more)
+            )
         }
     }
 }
