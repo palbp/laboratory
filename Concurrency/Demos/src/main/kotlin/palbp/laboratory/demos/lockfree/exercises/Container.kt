8f5f58ece366d228af67771c93a6e14c5882dde2
Paulo Pereira <palbp.giga@gmail.com>
2022-07-11 12:45:59 +0100

    Adds Container implementation and its tests

---
+++ Concurrency/Demos/src/main/kotlin/palbp/laboratory/demos/lockfree/exercises/Container.kt
@@ -0,0 +1,56 @@
+package palbp.laboratory.demos.lockfree.exercises
+
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * A possible solution to the following exercise.
+ *
+ * Implement, without using locks, a thread-safe version of the UnsafeContainer class that stores a set of values
+ * and the number of times those values can be consumed.
+ *
+ * class UnsafeValue<T>(val value: T, var Lives: Int)
+ * class UnsafeContainer<T>(private val values: Array<UnsafeValue<T>>){
+ *    private var index = 0
+ *    fun consume(): T? {
+ *       while(index < values.size) {
+ *          if (values[index].lives > 0) {
+ *             values[index].lives -= 1
+ *             return values[index].value
+ *          }
+ *          index += 1
+ *       }
+ *       return null
+ *    }
+ * }
+ *
+ * As an example, the container constructed by Container(arrayOf(Value("isel", 3), Value("pc", 4))) returns, through
+ * the consume method, the string "isel" three times and the string "pc" four times. After that, all calls to consume
+ * return null.
+ */
+
+class Value<T>(val value: T, initialLives: Int) {
+    val lives = AtomicInteger(initialLives)
+}
+
+class Container<T>(private val values: Array<Value<T>>){
+
+    private val index = AtomicInteger(0)
+
+    fun consume(): T? {
+        while(true) {
+            val observedIndex = index.get()
+            if (observedIndex >= values.size)
+                return null
+
+            val observedValue = values[observedIndex]
+            val observedLives = observedValue.lives.get()
+
+            if (observedLives != 0) {
+                if (observedValue.lives.compareAndSet(observedLives, observedLives - 1))
+                    return observedValue.value
+            } else {
+                index.compareAndExchange(observedIndex, observedIndex + 1)
+            }
+        }
+    }
+}
