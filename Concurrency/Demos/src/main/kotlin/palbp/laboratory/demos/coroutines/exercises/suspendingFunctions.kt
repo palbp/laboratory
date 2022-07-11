c5e6ab43fd94fa633f52917a75be2eec8cebb9f0
Paulo Pereira <palbp.giga@gmail.com>
2022-07-11 16:09:22 +0100

    Adds race function implementation and the corresponding tests

---
+++ Concurrency/Demos/src/main/kotlin/palbp/laboratory/demos/coroutines/exercises/suspendingFunctions.kt
@@ -0,0 +1,57 @@
+package palbp.laboratory.demos.coroutines.exercises
+
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+/**
+ *  Possible solution for the following exercise.
+ *
+ *  Implement the function with the following signature:
+ *
+ *  suspend fun race(f0: suspend () -> Int, f1: suspend () -> Int): Int
+ *
+ *  This function executes the functions passed as an argument in parallel, returning the value returned by the first
+ *  function to finish successfully. An execution of the race function should only end when the coroutines created in
+ *  its scope have finished. However, the race function must terminate as quickly as possible, by canceling the still
+ *  running coroutine, after one of the functions passed as an argument has successfully completed.
+ */
+
+/**
+ * The following solution leverages default cancellation behaviour.
+ */
+suspend fun race(f0: suspend () -> Int, f1: suspend () -> Int): Int {
+
+    val guard = Mutex()
+    var firstResult: Int? = null
+
+    try {
+        coroutineScope {
+            val parent = this
+            launch {
+                val result = f0()
+                guard.withLock {
+                    if (firstResult == null) {
+                        firstResult = result
+                        parent.cancel()
+                    }
+                }
+            }
+
+            launch {
+                val result = f1()
+                guard.withLock {
+                    if (firstResult == null) {
+                        firstResult = result
+                        parent.cancel()
+                    }
+                }
+            }
+        }
+    }
+    catch (_: CancellationException) { }
+    return guard.withLock { firstResult as Int }
+}
---
+++ Concurrency/Demos/src/test/kotlin/palbp/laboratory/demos/coroutines/exercises/SuspendingFunctionsTests.kt
@@ -0,0 +1,52 @@
+package palbp.laboratory.demos.coroutines.exercises
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
+
+private val logger = LoggerFactory.getLogger(SuspendingFunctionsTests::class.java)
+
+@OptIn(ExperimentalTime::class)
+class SuspendingFunctionsTests {
+
+    @Test
+    fun `race between fast f0 and slow f1 returns f0 result`() {
+        val f0Time = 1000
+        val f1Time = 5000
+        val elapsedTime = measureTime {
+            val result = runBlocking {
+                race(
+                    f0 = { delay(f0Time.toLong()); f0Time },
+                    f1 = { delay(f1Time.toLong()); f1Time }
+                )
+            }
+
+            assertEquals(f0Time, result)
+        }
+
+        assertTrue(elapsedTime.inWholeMilliseconds < f1Time)
+    }
+
+    @Test
+    fun `race between slow f0 and fast f1 returns f1 result`() {
+        val f0Time = 5000
+        val f1Time = 1000
+        val elapsedTime = measureTime {
+            val result = runBlocking {
+                race(
+                    f0 = { delay(f0Time.toLong()); f0Time },
+                    f1 = { delay(f1Time.toLong()); f1Time }
+                )
+            }
+
+            assertEquals(f1Time, result)
+        }
+
+        assertTrue(elapsedTime.inWholeMilliseconds < f0Time)
+    }
+}
