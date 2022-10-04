8221b8744c5739d816dcd90508abebabbd602f8f
Paulo Pereira <palbp.giga@gmail.com>
2022-10-04 18:57:54 +0100

    Adds draft implementation of the real quote service using okhttp

--- Mobile/Demos/QuoteOfDay/app/src/main/java/palbp/laboratory/demos/quoteofday/QuoteOfDayApplication.kt
+++ Mobile/Demos/QuoteOfDay/app/src/main/java/palbp/laboratory/demos/quoteofday/QuoteOfDayApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import kotlinx.coroutines.delay
 import palbp.laboratory.demos.quoteofday.daily.Quote
 import palbp.laboratory.demos.quoteofday.daily.QuoteService
+import palbp.laboratory.demos.quoteofday.daily.RealQuoteService
+import java.net.URL
 
 const val TAG = "QuoteOfDayDemo"
 
@@ -11,8 +13,10 @@ interface DependenciesContainer {
     val quoteService: QuoteService
 }
 
+private val quoteAPIHome = URL("https://184b-2001-690-2008-df53-d4d7-fc3e-10c4-b7b2.ngrok.io")
+
 class QuoteOfDayApplication : DependenciesContainer, Application() {
-    override val quoteService: QuoteService by lazy { FakeQuoteService() }
+    override val quoteService: QuoteService by lazy { RealQuoteService(quoteAPIHome) }
 }
 
 private class FakeQuoteService : QuoteService {
--- Mobile/Demos/QuoteOfDay/app/src/main/java/palbp/laboratory/demos/quoteofday/daily/QuoteService.kt
+++ Mobile/Demos/QuoteOfDay/app/src/main/java/palbp/laboratory/demos/quoteofday/daily/QuoteService.kt
@@ -1,5 +1,45 @@
 package palbp.laboratory.demos.quoteofday.daily
 
+import android.util.Log
+import okhttp3.*
+import palbp.laboratory.demos.quoteofday.TAG
+import java.io.IOException
+import java.net.URL
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
 interface QuoteService {
     suspend fun fetchQuote(): Quote
+}
+
+class RealQuoteService(private val quoteHome: URL) : QuoteService {
+
+    private val client by lazy { OkHttpClient() }
+
+    override suspend fun fetchQuote(): Quote {
+        val request = Request.Builder()
+            .url(quoteHome)
+            .build()
+
+        Log.v(TAG, "fetchQuote: before suspendCoroutine in Thread = ${Thread.currentThread().name}")
+        val quote = suspendCoroutine { continuation ->
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.v(TAG, "fetchQuote: onFailure in Thread = ${Thread.currentThread().name}")
+                    continuation.resumeWithException(e)
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.v(TAG, "fetchQuote: onResponse in Thread = ${Thread.currentThread().name}")
+                    continuation.resume(Quote("Yeahh", "Cool Kid"))
+                }
+            })
+
+        }
+
+        Log.v(TAG, "fetchQuote: after suspendCoroutine in Thread = ${Thread.currentThread().name}")
+        return quote
+    }
 }
