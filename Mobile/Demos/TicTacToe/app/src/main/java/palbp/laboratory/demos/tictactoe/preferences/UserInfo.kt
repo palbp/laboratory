9d978a546ab34299522ec929c78ba112813fddaf
Paulo Pereira <palbp.giga@gmail.com>
2022-11-08 18:10:18 +0000

    Adds UserInfo repository and its tests

--- Mobile/Demos/TicTacToe/app/src/main/java/palbp/laboratory/demos/tictactoe/preferences/UserInfo.kt
+++ Mobile/Demos/TicTacToe/app/src/main/java/palbp/laboratory/demos/tictactoe/preferences/UserInfo.kt
@@ -1,5 +1,11 @@
 package palbp.laboratory.demos.tictactoe.preferences
 
+/**
+ * Represents the user information.
+ *
+ * @property [nick] the user's nick name
+ * @property [moto] the user's moto, if he has one
+ */
 data class UserInfo(val nick: String, val moto: String? = null) {
     init {
         require(nick.isNotBlank())
