01d8cf404a3b891c3d4946395e7fd588fe2e4c2c
Paulo Pereira <palbp.giga@gmail.com>
2022-11-08 18:12:22 +0000

    Adds User repository dependency to the service locator - Adds test that verifies if the preferences screen is displayed if no user info exists in the repo. The test is currently failing. - The test that verifies if the lobby is displayed when the user info exists is passing.

--- Mobile/Demos/TicTacToe/app/src/androidTest/java/palbp/laboratory/demos/tictactoe/TicTacToeTestRunner.kt
+++ Mobile/Demos/TicTacToe/app/src/androidTest/java/palbp/laboratory/demos/tictactoe/TicTacToeTestRunner.kt
@@ -3,11 +3,20 @@ package palbp.laboratory.demos.tictactoe
 import android.app.Application
 import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import io.mockk.every
+import io.mockk.mockk
+import palbp.laboratory.demos.tictactoe.preferences.UserInfo
+import palbp.laboratory.demos.tictactoe.preferences.UserInfoRepository
 
 /**
  * The service locator to be used in the instrumented tests.
  */
-class TicTacToeTestApplication : DependenciesContainer, Application()
+class TicTacToeTestApplication : DependenciesContainer, Application() {
+    override var userInfoRepo: UserInfoRepository =
+        mockk {
+            every { userInfo } returns UserInfo("nick", "moto")
+        }
+}
 
 @Suppress("unused")
 class TicTacToeTestRunner : AndroidJUnitRunner() {
--- Mobile/Demos/TicTacToe/app/src/androidTest/java/palbp/laboratory/demos/tictactoe/main/MainActivityTests.kt
+++ Mobile/Demos/TicTacToe/app/src/androidTest/java/palbp/laboratory/demos/tictactoe/main/MainActivityTests.kt
@@ -4,13 +4,12 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.*
 import org.junit.Rule
-import palbp.laboratory.demos.tictactoe.main.MainActivity
+import org.junit.Test
+import org.junit.runner.RunWith
+import palbp.laboratory.demos.tictactoe.TicTacToeTestApplication
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTests {
@@ -25,7 +24,9 @@ class MainActivityTests {
     }
 
     @Test
-    fun pressing_start_button_navigates_to_lobby() {
+    fun pressing_play_navigates_to_lobby_if_user_info_exists() {
+
+        // Arrange not required. Default repo always returns a UserInfo instance
 
         // Act
         testRule.onNodeWithTag("PlayButton").performClick()
@@ -33,5 +34,29 @@ class MainActivityTests {
 
         // Assert
         testRule.onNodeWithTag("LobbyScreen").assertExists()
+    }
+
+    @Test
+    fun pressing_start_button_navigates_to_preferences_if_user_info_does_not_exist() {
+
+        // Arrange
+        val testApplication: TicTacToeTestApplication = InstrumentationRegistry
+            .getInstrumentation()
+            .targetContext
+            .applicationContext as TicTacToeTestApplication
+
+        val defaultUserInfoRepo = testApplication.userInfoRepo
+
+        try {
+            // Act
+            testRule.onNodeWithTag("PlayButton").performClick()
+            testRule.waitForIdle()
+
+            // Assert
+            testRule.onNodeWithTag("PreferencesScreen").assertExists()
+        }
+        finally {
+            testApplication.userInfoRepo = defaultUserInfoRepo
+        }
     }
 }
