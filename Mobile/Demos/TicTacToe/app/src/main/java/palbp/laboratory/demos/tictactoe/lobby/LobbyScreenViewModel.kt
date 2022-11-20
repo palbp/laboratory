a4c05aa10103686f822886ff15491a37424f0c24
Paulo Pereira <palbp.giga@gmail.com>
2022-11-20 01:08:02 +0000

    Updates dependencies

--- Mobile/Demos/TicTacToe/app/src/main/java/palbp/laboratory/demos/tictactoe/lobby/LobbyScreenViewModel.kt
+++ Mobile/Demos/TicTacToe/app/src/main/java/palbp/laboratory/demos/tictactoe/lobby/LobbyScreenViewModel.kt
@@ -10,6 +10,7 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import palbp.laboratory.demos.tictactoe.TAG
 import palbp.laboratory.demos.tictactoe.preferences.UserInfo
@@ -20,17 +21,19 @@ import palbp.laboratory.demos.tictactoe.preferences.UserInfo
 class LobbyScreenViewModel(val lobby: Lobby) : ViewModel() {
 
     private val _players = MutableStateFlow<List<UserInfo>>(emptyList())
-    val players: StateFlow<List<UserInfo>>
-        get() = _players
+    val players = _players.asStateFlow()
 
     private var lobbyMonitor: Job? = null
 
     fun enterLobby() {
         if (lobbyMonitor == null) {
             lobbyMonitor = viewModelScope.launch {
+                Log.v(TAG, "viewModelScope coroutine")
                 lobby.players.collect {
+                    Log.v(TAG, "collect block starts")
                     Log.v(TAG, "ViewModel is collecting from the lobby: $it")
                     _players.value = it
+                    Log.v(TAG, "collect block ends")
                 }
             }
         }
