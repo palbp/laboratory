84df1fe3be31ae2daf1414131d91e960a03ae224
Paulo Pereira <palbp.giga@gmail.com>
2024-01-05 00:33:38 +0000

    Adds SimplexLudum Kotlin Multiplatform project, created with the provided wizard

---
+++ Mobile/SimplexLudum/composeApp/src/androidMain/kotlin/palbp/laboratory/simplexludum/previews/AppPreview.kt
@@ -0,0 +1,11 @@
+package palbp.laboratory.simplexludum.previews
+
+import App
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun AppPreview() {
+    App()
+}
